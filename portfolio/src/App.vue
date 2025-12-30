58486d8b6a8e806dc2cc6413f9da9464b3fed278
HeeJoo <k.lbtkr@gmail.com>
2025-12-30 12:33:02 +0900

    vue.js portfoilo 노트북 작업

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,14 +1,29 @@
 <script setup>
-
+    import Title from './components/Title.vue';
 </script>
 
 <template>
-    <div class="parent">
-        <h1 class="display-1">App</h1>
-        <button class="btn btn-primary">button</button>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Portfolio</a>
+                </li>
+            </ul>
+        </div>
     </div>
+</nav>
 
-    <h1>heading 1</h1>
+<Title></Title>
+
 </template>
 
 <style scoped lang="scss">
---
+++ portfolio/src/components/Title.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+<template>
+    <section id="title">
+        <div class="container">
+            <div class="detail row">
+                <figure class="col">
+                    <img src="./assets/images/title.png" alt="title">
+                </figure>
+                <div class="text col">
+                    <h2>Hello,<br>I'm Heejoo</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<style scoped lang="scss">
+    
+</style>
