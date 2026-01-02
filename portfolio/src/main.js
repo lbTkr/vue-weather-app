5d762194775c53cdc3a65841b50bb05a6b652e96
희주/HeeJu <K.lbTkr@gmail.com>
2026-01-02 16:42:14 +0900

    Delete portfolio directory

--- portfolio/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import router from './router/router.js';
-
-createApp(App).use(router).mount('#app');
