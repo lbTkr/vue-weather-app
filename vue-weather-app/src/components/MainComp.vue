1e93f2b6af770fb3df8f4afcdb58c482edf5d362
강희주 <k.lbtkr@gmail.com>
2026-01-02 21:11:44 +0900

    Implement MainComp and WeatherInfo components; remove SearchBar from App.vue

---
+++ vue-weather-app/src/components/MainComp.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <SearchBar />
+        <WeatherInfo />
+    </div>
+</template>
+
+<script setup>
+    import SearchBar from './SearchBar.vue';
+    import WeatherInfo from './WeatherInfo.vue';
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vue-weather-app/src/components/SearchBar.vue
+++ vue-weather-app/src/components/SearchBar.vue
@@ -16,17 +16,31 @@
 </script>
 
 <style lang="scss" scoped>
-.search-bar {
-    form {
-        .form-group {
-            button {
-                border:none;
-                background:transparent;
-                .icon {
-                    color:#000;
+    .search-bar {
+        padding: 8px 20px;
+        border-radius: 40px;
+        background: #fff;
+        border: 1px solid #ccc;
+        padding: 0 20px;
+    
+        form {
+            .form-group {
+                display: flex;
+                input {
+                    width: 100%;
+                    border: none;
+                    padding: 1em 0;
+                    font-size: 18px;
+                    outline: none;
+                    font-size: 14px;
+                }
+                button {
+                    background: transparent;
+                    border: none;
+                    &:hover { cursor: pointer; }
+                    .icon { font-size: 24px; }
                 }
             }
         }
     }
-}
 </style>
